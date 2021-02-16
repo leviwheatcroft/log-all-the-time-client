33ca3695fc6b1226725c5893462f202852d9c4e8
Levi Wheatcroft <levi@wheatcroft.com.au>
2021-02-16 19:33:19 +0800

    remove extraneous components

--- src/components/EntryEditInput/_index.vue
+++
@@ -1,3 +0,0 @@
-<template lang="pug" src="./EntryEditInput.pug">foo</template>
-<script src="./EntryEditInput.js"></script>
-<style lang="less" scoped src="./EntryEditInput.less"></style>
--- src/components/EntryNew/Submit/_index.vue
+++
@@ -1,3 +0,0 @@
-<template lang="pug" src="./Submit.pug">foo</template>
-<script src="./Submit.js"></script>
-<style lang="less" scoped src="./Submit.less"></style>
