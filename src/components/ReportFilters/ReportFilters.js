6ff1d29348500aaa8c1f482371cd155c4fb1d6cf
Levi Wheatcroft <levi@wheatcroft.com.au>
2021-02-18 06:28:14 +0800

    add vue2-date-picker to ReportFilters

--- src/components/ReportFilters/ReportFilters.js
+++ src/components/ReportFilters/ReportFilters.js
@@ -1,6 +1,16 @@
+const { default: DatePicker } = require('vue2-datepicker')
+
 const ReportFilters = {
+  components: {
+    DatePicker
+  },
+  computed: {
+    dateFrom () { return this.dateRange[0] },
+    dateTo () { return this.dateRange[0] }
+  },
   data () {
     return {
+      dateRange: [new Date(), new Date()],
       dateFrom: '',
       dateTo: '',
       tags: ''
