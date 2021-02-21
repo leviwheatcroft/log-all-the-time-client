1212bc08a9da008315cdebe24528cf21356f4adc
Levi Wheatcroft <levi@wheatcroft.com.au>
2021-02-21 08:03:00 +0800

    split EntryInput date, duration, description, tags. basic tag selector

--- src/components/EntryNew/EntryNew.js
+++ src/components/EntryNew/EntryNew.js
@@ -1,61 +1,75 @@
-const {
-  mutations: {
-    EntryAddM
-  },
-  queries: {
-    EntryQ
-  }
-} = require('../../apollo')
-
-const { resolvers: { resolve } } = require('../../lib')
+// const {
+//   mutations: {
+//     EntryAddM
+//   },
+//   queries: {
+//     EntryQ
+//   }
+// } = require('../../apollo')
+//
+// const { resolvers: { resolve } } = require('../../lib')
+const { default: EntryTagInput } = require('../EntryTagInput')
 
 const EntryNew = {
+  components: {
+    EntryTagInput
+  },
   data () {
     return {
-      raw: ''
+      date: new Date(),
+      duration: '',
+      description: '',
+      tagNames: ['one', 'two', 'three']
     }
   },
   methods: {
+    tagAdded (tag) {
+      this.tagNames.push(tag)
+    },
+    tagRemoved (tag) {
+      const idx = this.tagNames.findIndex((t) => t === tag)
+      this.tagNames.splice(idx, 1)
+    },
     async submit () {
-      const entry = resolve({
-        date: null,
-        timeStart: null,
-        timeEnd: null,
-        duration: null,
-        tags: [],
-        raw: this.$data.raw,
-        description: this.$data.raw
-      })
-      await this.$apollo.mutate({
-        mutation: EntryAddM,
-        variables: {
-          entry
-        },
-        update: (store, ctx) => {
-          const {
-            data: {
-              EntryAddM: result
-            }
-          } = ctx
-
-          const data = store.readQuery({ query: EntryQ })
-          data.EntryQ.unshift(result)
-          store.writeQuery({ query: EntryQ, data })
-        },
-        optimisticResponse: {
-          __typename: 'Mutation',
-          EntryAddM: {
-            id: -1,
-            __typename: 'Entry',
-            ...entry,
-            tags: entry.tags.map((tag) => ({
-              __typename: 'Tag',
-              id: -1,
-              tag
-            }))
-          }
-        }
-      })
+    //   const entry = resolve({
+    //     date: null,
+    //     timeStart: null,
+    //     timeEnd: null,
+    //     duration: null,
+    //     tags: [],
+    //     raw: this.$data.raw,
+    //     description: this.$data.raw
+    //   })
+    //   await this.$apollo.mutate({
+    //     mutation: EntryAddM,
+    //     variables: {
+    //       entry
+    //     },
+    //     update: (store, ctx) => {
+    //       const {
+    //         data: {
+    //           EntryAddM: result
+    //         }
+    //       } = ctx
+    //
+    //       const data = store.readQuery({ query: EntryQ })
+    //       data.EntryQ.unshift(result)
+    //       store.writeQuery({ query: EntryQ, data })
+    //     },
+    //     optimisticResponse: {
+    //       __typename: 'Mutation',
+    //       EntryAddM: {
+    //         id: -1,
+    //         __typename: 'Entry',
+    //         ...entry,
+    //         tags: entry.tags.map((tag) => ({
+    //           __typename: 'Tag',
+    //           id: -1,
+    //           tag
+    //         }))
+    //       }
+    //     }
+    //   })
     }
   }
 }
---
+++ src/components/EntryTagInput/_index.vue
@@ -0,0 +1,3 @@
+<template lang="pug" src="./EntryTagInput.pug">foo</template>
+<script src="./EntryTagInput.js"></script>
+<style lang="less" scoped src="./EntryTagInput.less"></style>
