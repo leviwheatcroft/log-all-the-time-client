904cb4e8862b9001fdea2ab10aee3f34145c0711
Levi Wheatcroft <levi@wheatcroft.com.au>
2021-03-14 07:34:56 +0800

    UserSelector don't send unnecessary queries

--- src/components/selectors/UserSelector/UserSelector.js
+++ src/components/selectors/UserSelector/UserSelector.js
@@ -1,3 +1,4 @@
+const _throttle = require('lodash/throttle')
 const { default: Selector } = require('../Selector')
 const {
   IconUser
@@ -15,23 +16,23 @@ const {
 } = require('../../../apollo')
 
 const UserSelector = {
-  apollo: {
-    itemSuggestions: {
-      query: UserPartialQ,
-      skip () { return this.itemPartial.length < 3 },
-      throttle: 500,
-      update ({ UserPartialQ: itemSuggestions }) {
-        itemSuggestions = itemSuggestions.map((i) => {
-          return {
-            ...i,
-            itemName: i.username
-          }
-        })
-        return itemSuggestions
-      },
-      variables () { return { userPartial: this.itemPartial } }
-    }
-  },
+  // apollo: {
+  //   itemSuggestions: {
+  //     query: UserPartialQ,
+  //     skip () { return this.itemPartial.length < 3 },
+  //     throttle: 500,
+  //     update ({ UserPartialQ: itemSuggestions }) {
+  //       itemSuggestions = itemSuggestions.map((i) => {
+  //         return {
+  //           ...i,
+  //           itemName: i.username
+  //         }
+  //       })
+  //       return itemSuggestions
+  //     },
+  //     variables () { return { userPartial: this.itemPartial } }
+  //   }
+  // },
   components: {
     IconUser,
     Selector
@@ -55,7 +56,57 @@ const UserSelector = {
   methods: {
     inputItemPartialHandler (itemPartial) {
       this.itemPartial = itemPartial
+
+      const {
+        itemSuggestions,
+        lastItemPartial,
+        loading
+      } = this
+
+      // don't send if we don't have enough text to generate meaningful results
+      if (itemPartial.length < 3)
+        return
+
+      // don't send if there's already a query in flight
+      if (loading)
+        return
+
+      // send if we haven't done the query at least once since reset
+      if (!lastItemPartial)
+        return this.itemSuggestionsQuery()
+
+      // send if we have any suggests from the last query
+      // even if there's only one remaining suggestion, we still need to query
+      // again in case the additional text excludes the one suggestion we have
+      if (itemSuggestions.length)
+        return this.itemSuggestionsQuery()
+
+      // send if the tagPartial sent with the last query is not included
+      // in the current tagPartial. This accounts for deleting text.
+      if (!itemPartial.includes(lastItemPartial))
+        return this.itemSuggestionsQuery()
     },
+    // itemSuggestionsQuery can't be included as an apollo smartQuery from
+    // vue-apollo because we need to micro-manage whether the query will be
+    // sent, and the skip() functionality doesn't work for this use case
+    itemSuggestionsQuery: _throttle(async function itemSuggestionsQuery () {
+      this.lastItemPartial = this.itemPartial
+      this.loading += 1
+      const result = await this.$apollo.query({
+        query: UserPartialQ,
+        loadingKey: 'loading',
+        variables: { userPartial: this.itemPartial }
+      })
+      let { data: { UserPartialQ: itemSuggestions } } = result
+      itemSuggestions = itemSuggestions.map((i) => {
+        return {
+          ...i,
+          itemName: i.username
+        }
+      })
+      this.itemSuggestions = itemSuggestions
+      this.loading -= 1
+    }, 500),
     resetHandler () {}
   },
   props: {
