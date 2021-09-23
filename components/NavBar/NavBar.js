58e7a42f84f020ef654f54a4bd5d5987d627ab9d
Levi Wheatcroft <levi@wheatcroft.com.au>
2021-09-23 16:20:42 +0800

    improved NavBar

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -3,6 +3,9 @@ const NavBar = {
   computed: {
     username () {
       return this.$store.state.user.username
+    },
+    gravatar () {
+      return this.$store.state.user.gravatar
     }
   }
 }
--- store/user.js
+++ store/user.js
@@ -1,12 +1,14 @@
 export const state = () => ({
   id: false,
-  username: false
+  username: false,
+  gravatar: false,
 })
 
 export const mutations = {
   logIn (state, user) {
     state.id = user.id
     state.username = user.username
+    state.gravatar = user.gravatar
   },
   logOut (state) {
     state.id = false
