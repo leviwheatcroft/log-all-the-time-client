698f5ac45c173ca2275761d19104611c4698ab55
Levi Wheatcroft <levi@wheatcroft.com.au>
2021-10-23 04:46:05 +0800

    graphql uri

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  APOLLO_URI: 'http://localhost:4000',
+  APOLLO_URI: 'http://localhost:4000/graphql',
   ACCESS_TOKEN_COOKIE_NAME: 'lattAccessToken',
   REFRESH_TOKEN_COOKIE_NAME: 'lattRefreshToken'
 }
